777111c6ef784647e2ab4bd66dd6704b99b7d155
Kawaldeep Singh <kawaldeep.singh@indipe.in>
2024-01-12 21:04:53 +0530

    feat(payment): Add UPI app query and transaction initiation
    This commit introduces a new feature to query all installed UPI packages on the user's mobile device. Additionally, it supports initiating transactions with the selected UPI package.

--- android/src/main/kotlin/com/example/quantupi/QuantupiPlugin.kt
+++ android/src/main/kotlin/com/example/quantupi/QuantupiPlugin.kt
@@ -3,7 +3,12 @@ package com.example.quantupi
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import android.net.Uri
+import android.os.Build
 import android.util.Log
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding
@@ -13,21 +18,30 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.PluginRegistry.ActivityResultListener
-
+import java.io.ByteArrayOutputStream
+import android.util.Base64
 
 /** QuantupiPlugin  */
 class QuantupiPlugin : FlutterPlugin, MethodCallHandler, ActivityResultListener, ActivityAware {
+  private lateinit var channel : MethodChannel
   private var uniqueRequestCode = 3498
   private var finalResult: MethodChannel.Result? = null
   private var activity: Activity? = null
+  private var hasResponded = false
+  private var exception = false
+
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPluginBinding) {
-    val channel = MethodChannel(flutterPluginBinding.binaryMessenger, "quantupi")
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "quantupi")
     channel.setMethodCallHandler(this)
   }
 
   override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+//    hasResponded = false
+//    this.result = result
+    finalResult = result
     when (call.method) {
       "startTransaction" -> startUpiTransaction(result, call)
+      "getInstalledUpiApps" -> getInstalledUpiApps(result)
       else -> result.notImplemented()
     }
   }
@@ -37,38 +51,156 @@ class QuantupiPlugin : FlutterPlugin, MethodCallHandler, ActivityResultListener,
     call: MethodCall,
   ) {
     val url = call.argument<String>("url")
+    val app: String? = call.argument("app")
 
     /// Check if url is valid upi url
-    if (url == null || !url.startsWith("upi://")) {
-      result.error("FAILED", "invalid_parameters", null)
-      return
-    }
+//    if (url == null || !url.startsWith("upi://")) {
+//      result.error("FAILED", "invalid_parameters", null)
+//      return
+//    }
 
     try {
       /// Storing result like this is a bad idea since this can cause issues in case of multiple
       // transactions involving multiple UPI Apps, however in practice this is unlikely to happen.
       finalResult = result
-      val intent = createUpiIntent(url)
+      exception = false
+      val intent = createUpiIntent(url,app)
       val packageManager: PackageManager = activity!!.packageManager
       if (intent.resolveActivity(packageManager) == null) {
         result.error("FAILED", "No UPI Apps found", null)
         return
       }
+//      if (packageManager?.let { intent.resolveActivity(it) } == null) {
+//        success("activity_unavailable")
+//        return
+//      }
 
       activity!!.startActivityForResult(intent, uniqueRequestCode)
     } catch (ex: Exception) {
+//      Log.e("quant_upi", ex.toString())
+//      success("failed_to_open_app")
+
+//      var msg=ex.message
+//      result.success("${msg}")
+      exception = true
+
       result.error("FAILED", "invalid_parameters", null)
     }
   }
 
-  private fun createUpiIntent(url: String?): Intent {
+  private fun createUpiIntent(url: String?, app: String?): Intent {
     val uri = Uri.parse(url)
     val intent = Intent(Intent.ACTION_VIEW)
     intent.data = uri
+    intent.setPackage(app)
     return intent
   }
 
-  override fun onDetachedFromEngine(binding: FlutterPluginBinding) {}
+  private fun getInstalledUpiApps(result: MethodChannel.Result) {
+    val packages: MutableList<Map<String, Any>> = ArrayList()
+    val intent = Intent(Intent.ACTION_VIEW)
+    Log.d("quantupi_fork", "getInstalledUpiApps")
+    val uriBuilder = Uri.Builder()
+    uriBuilder.scheme("upi").authority("pay")
+//    uriBuilder.appendQueryParameter("pa", "test@ybl")
+//    uriBuilder.appendQueryParameter("pn", "Test")
+//    uriBuilder.appendQueryParameter("tn", "Get All Apps")
+//    uriBuilder.appendQueryParameter("am", "1.0")
+//    uriBuilder.appendQueryParameter("cr", "INR")
+    val uri = uriBuilder.build()
+    intent.data = uri
+    val packageManager = activity!!.packageManager
+    val resolveInfoList:List<ResolveInfo>
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+      resolveInfoList = packageManager
+              .queryIntentActivities(intent, PackageManager.ResolveInfoFlags.of(PackageManager.MATCH_DEFAULT_ONLY.toLong()))
+    }else{
+      resolveInfoList = packageManager
+              .queryIntentActivities(intent, 0);
+    }
+
+    for (resolveInfo in resolveInfoList) {
+      try {
+        // Get Package name of the app.
+        val packageName = resolveInfo.activityInfo.packageName
+
+        // Get Actual name of the app to display
+        var name: String
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+          name= packageManager.getApplicationLabel(
+                  packageManager.getApplicationInfo(
+                          packageName,
+                          PackageManager.ApplicationInfoFlags.of(PackageManager.GET_META_DATA.toLong())
+                  )
+          ) as String
+        }else{
+          name = packageManager.getApplicationLabel(
+                  packageManager.getApplicationInfo(
+                          packageName,
+                          PackageManager.GET_META_DATA
+                  )
+
+          ) as String
+        }
+
+
+        // Get app icon as Drawable
+        val dIcon = packageManager.getApplicationIcon(packageName)
+
+        // Convert the Drawable Icon as Bitmap.
+        val bIcon = getBitmapFromDrawable(dIcon)
+
+        // Convert the Bitmap icon to byte[] received as Uint8List by dart.
+        val stream = ByteArrayOutputStream()
+        bIcon.compress(Bitmap.CompressFormat.PNG, 100, stream)
+        val icon = Base64.encodeToString(stream.toByteArray(), Base64.NO_WRAP)
+
+        // Put everything in a map
+        val m: MutableMap<String, Any> = HashMap()
+        m["packageName"] = packageName
+        m["name"] = name
+        m["icon"] = icon
+
+        // Add this app info to the list.
+        packages.add(m)
+
+      } catch (e: Exception) {
+        e.printStackTrace()
+        Log.e("quant_upi", e.toString())
+        result.error("getInstalledUpiApps", "exception", e)
+      }
+    }
+
+    result.success(packages)
+  }
+  private fun encodeToBase64(image: Bitmap): String? {
+    val byteArrayOS = ByteArrayOutputStream()
+    image.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOS)
+    return Base64.encodeToString(byteArrayOS.toByteArray(), Base64.NO_WRAP)
+  }
+  // It converts the Drawable to Bitmap. There are other inbuilt methods too.
+  private fun getBitmapFromDrawable(drawable: Drawable): Bitmap {
+    val bmp: Bitmap = Bitmap.createBitmap(
+            drawable.intrinsicWidth,
+            drawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+    )
+    val canvas = Canvas(bmp)
+    drawable.setBounds(0, 0, canvas.width, canvas.height)
+    drawable.draw(canvas)
+    return bmp
+  }
+
+  private fun success(o: String) {
+    if (!hasResponded) {
+      hasResponded = true
+      finalResult?.success(o)
+    }
+  }
+
+  override fun onDetachedFromEngine(binding: FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
 
   // On receiving the response.
   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
@@ -125,7 +257,18 @@ class QuantupiPlugin : FlutterPlugin, MethodCallHandler, ActivityResultListener,
     binding.addActivityResultListener(this)
   }
 
-  override fun onDetachedFromActivityForConfigChanges() {}
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {}
-  override fun onDetachedFromActivity() {}
+//  override fun onDetachedFromActivityForConfigChanges() {}
+//  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {}
+//  override fun onDetachedFromActivity() {}
+  override fun onDetachedFromActivityForConfigChanges() {
+    activity = null;
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    activity = binding.activity;
+  }
+
+  override fun onDetachedFromActivity() {
+    activity = null;
+  }
 }
--- lib/quantupi.dart
+++ lib/quantupi.dart
@@ -1,16 +1,16 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:developer';
+import 'package:flutter/material.dart';
 
 import 'package:flutter/services.dart';
-
+import 'upi_app_model.dart';
 /// TODO: move to platform channel interface
 class Quantupi {
-  Quantupi({required this.url}) {
+  Quantupi() {
     _channel.setMethodCallHandler(_fromNative);
   }
   static const _channel = MethodChannel('quantupi');
-
-  final String url;
 
   Future<void> _fromNative(MethodCall call) async {
     if (call.method == 'callTestResuls') {
@@ -18,13 +18,12 @@ class Quantupi {
     }
   }
 
-  Future<String> startTransaction() async {
+  Future<String> startTransaction( String packageName,
+       String url,) async {
     try {
       if (Platform.isAndroid) {
-        final String response =
-            await _channel.invokeMethod('startTransaction', {
-          'url': url,
-        });
+        final String response = await _channel
+            .invokeMethod('startTransaction', {'url': url, 'app': packageName});
         return response;
       } else if (Platform.isIOS) {
         final result = await _channel.invokeMethod(
@@ -48,6 +47,81 @@ class Quantupi {
       }
 
       rethrow;
+    }
+  }
+
+  Future<List<UpiApp>> getUpiApps() async {
+    List<UpiApp>? upiList =
+    await getAllUpiApps();
+    if (upiList.isNotEmpty) {
+      return upiList;
+    }
+    return [];
+  }
+
+  Future<List<UpiApp>> getAllUpiApps() async {
+    final List<Map>? apps = await _channel.invokeListMethod<Map>('getInstalledUpiApps');
+    List<UpiApp> upiIndiaApps = [];
+    apps?.forEach((Map app) {
+      var icon= app['icon'];
+      if (app['packageName'] == "in.org.npci.upiapp" ||
+          app['packageName'] == "com.phonepe.app" ||
+          app['packageName'] == "net.one97.paytm" ||
+          app['packageName'] == "com.phonepe.simulator"
+
+      ///Coming soon
+      ///app['packageName'] == "com.google.android.apps.nbu.paisa.user"
+      ///app['packageName'] == "in.amazon.mShop.android.shopping"
+      // app['packageName'] == "com.freecharge.android" ||
+      // app['packageName'] == "com.axis.mobile" ||
+      // app['packageName'] == "com.infrasofttech.centralbankupi" ||
+      // app['packageName'] == "com.infra.boiupi" ||
+      // app['packageName'] == "com.lcode.corpupi" ||
+      // app['packageName'] == "com.lcode.csbupi" ||
+      // app['packageName'] == "com.dbs.in.digitalbank" ||
+      // app['packageName'] == "com.equitasbank.upi" ||
+      // app['packageName'] == "com.mgs.hsbcupi" ||
+      // app['packageName'] == "com.csam.icici.bank.imobile" ||
+      // app['packageName'] == "com.lcode.smartz" ||
+      // app['packageName'] == "com.mgs.induspsp" ||
+      // app['packageName'] == "com.msf.kbank.mobile" ||
+      // app['packageName'] == "com.hdfcbank.payzapp" ||
+      // app['packageName'] == "com.Version1" ||
+      // app['packageName'] == "com.psb.omniretail" ||
+      // app['packageName'] == "com.rblbank.mobank" ||
+      // app['packageName'] == "com.lcode.ucoupi" ||
+      // app['packageName'] == "com.ultracash.payment.customer" ||
+      // app['packageName'] == "com.YesBank" ||
+      // app['packageName'] == "com.bankofbaroda.upi" ||
+      // app['packageName'] == "com.myairtelapp" ||
+      // app['packageName'] == "com.dreamplug.androidapp" ||
+      // app['packageName'] == "com.sbi.upi"
+      ) {
+        // || app['packageName']== "com.whatsapp"
+        // || app['packageName']== "com.whatsapp.w4b") {
+        upiIndiaApps.add(UpiApp.fromMap(Map<String, dynamic>.from(app)));
+      }
+    });
+    return upiIndiaApps;
+  }
+
+  /// Navigates to the App Store to download the specified UPI payment app.
+  ///
+  /// Parameters:
+  /// - [url]: The App Store URL for the UPI payment app.
+  ///
+  /// Returns:
+  /// A [Future] that completes after navigating to the App Store.
+  Future<void> navigateToAppstore(String? url) async {
+    try {
+      await _channel.invokeMethod(
+        'navigateToAppstore',
+        {
+          'uri': url,
+        },
+      );
+    } catch (error) {
+      throw Exception(error);
     }
   }
 }
