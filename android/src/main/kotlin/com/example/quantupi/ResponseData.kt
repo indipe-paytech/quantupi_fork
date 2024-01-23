487048ca57f1cbd0eff27d0d6e1f00a09903153b
Ibrahim Mubarak Hussain <47777720+ibrahim-mubarak@users.noreply.github.com>
2024-01-23 13:18:43 +0530

    Delete android/src/main/kotlin/com/example/quantupi/ResponseData.kt

--- android/src/main/kotlin/com/example/quantupi/ResponseData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.quantupi
-
-data class ResponseData(
-        val txnId: String?,
-        val txnRef: String?,
-        val transactionId: String?,
-        val status: String?,
-        val responseCode: String?
-)
